9afa813ef12b8e30740ed6e4bf24d47db89dc94e
Luciana Cores Formighieri <38226768+Lucores@users.noreply.github.com>
2026-02-19 13:28:03 -0300

    Add custom commands for e-commerce tests

---
+++ cypress/support/commands.js
@@ -0,0 +1,6 @@
+// Custom commands for Cypress
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('input[name=email]').type(email);
+    cy.get('input[name=password]').type(password);
+    cy.get('button[type=submit]').click();
+});
